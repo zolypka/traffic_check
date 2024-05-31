3980d65edb7f2d448dd30a365367055d1292c304
zolypka <161860029+zolypka@users.noreply.github.com>
2024-05-31 13:32:23 +0300

    Create traffic_tests.py

---
+++ traffic_tests.py
@@ -0,0 +1,24 @@
+import os
+import time
+import psutil
+from prettytable import PrettyTable
+from prettytable import DOUBLE_BORDER
+from traffic_but_help_me import getSize, getProcessNetworkUsage
+
+def test_getSize():
+    assert getSize(1023) == '1023.0bytes'
+    assert getSize(1024) == '1.0KB'
+    assert getSize(1048576) == '1.0MB'
+    assert getSize(1073741824) == '1.0GB'
+    assert getSize(1099511627776) == '1.0TB'
+
+def test_getProcessNetworkUsage():
+    networkUsage = getProcessNetworkUsage()
+    assert isinstance(networkUsage, dict)
+    for key, value in networkUsage.items():
+        assert isinstance(key, str)
+        assert isinstance(value, psutil._common.snetio)
+
+def test_whole_program():
+    test_getSize()
+    test_getProcessNetworkUsage()
